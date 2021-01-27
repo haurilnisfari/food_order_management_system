85cc4434353ac5f1e38260f84b1dd4df3d31d8d2
Hauril Maulida Nisfari <hauril.nisfari@gmail.com>
2021-01-27 19:30:10 +0700

    create model, controller, view of menu items

---
+++ app/controllers/menu_items_controller.rb
@@ -0,0 +1,47 @@
+class MenuItemsController < ApplicationController
+
+    def new
+        @menu = MenuItem.new
+    end
+
+    def create
+        flash[:notice] = "Menu Item has been created"
+        menu = MenuItem.new(resource_params)
+        menu.save
+        redirect_to menu_items_path
+    end
+
+    def index
+        @menus = MenuItem.all
+    end
+    
+    def edit
+        @menu = MenuItem.find(params[:id])
+    end
+
+    def update
+        flash[:notice] = "Menu Item has been updated"
+        menu = MenuItem.find(params[:id])
+        menu.update(resource_params)
+        redirect_to menu_items_path
+    end
+
+
+    def show
+        @menu = MenuItem.find(params[:id])
+    end
+
+    def destroy
+        flash[:notice] = "Menu Item has been deleted"
+        @menu = MenuItem.find(params[:id])
+        @menu.destroy
+        redirect_to menu_items_path
+    end
+
+    private
+
+    def resource_params
+        params.require(:menu_item).permit(:name, :price, :note)
+      end
+
+end
---
+++ app/models/menu_item.rb
@@ -0,0 +1,5 @@
+class MenuItem < ApplicationRecord
+    has_many :menu_categories
+    has_many :categories, through: :menu_categories
+  end
+  
