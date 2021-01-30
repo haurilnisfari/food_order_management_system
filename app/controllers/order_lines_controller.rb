c45da40e4acfa21926d19005744dfbed50b66dc1
Hauril Maulida Nisfari <hauril.nisfari@gmail.com>
2021-01-31 02:34:29 +0700

    order lines

--- app/controllers/order_lines_controller.rb
+++ app/controllers/order_lines_controller.rb
@@ -1,5 +1,18 @@
 class OrderLinesController < ApplicationController
 
+  
+  def new
+    @line = OrderLine.new
+    @orders = Order.all
+    @menus = MenuItem.all
+  end
+
+  def create
+    line = OrderLine.new(resource_params)
+    line.save
+  end
+
+
   def index
     @lines = OrderLine.all
     @menus = MenuItem.all
